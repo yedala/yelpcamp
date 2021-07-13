3b5fa059b0fa6e14b0e267d4827ea9709280a74c
Aravind <aravindyedala49@gmail.com>
2021-07-13 15:51:10 +0530

    server side validations

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const path=require('path')
+const Joi=require('joi');
 const ejsMate=require('ejs-mate');
 const mongoose=require('mongoose');
 const methodOverride=require('method-override');
@@ -37,7 +38,22 @@ app.get('/campgrounds/new',(req,res)=>{
     res.render('campgrounds/new');
 })
 app.post('/campgrounds',catchAsync(async(req,res,next)=>{
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data,400');
+    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data,400');
+    const campgroundSchema=Joi.object({
+        campground:Joi.object({
+            title:Joi.string().required(),
+            price:Joi.number().required().min(0),
+            image:Joi.string().required(),
+            location:Joi.string().required(),
+            description:Joi.string().required()
+        }).required()
+    })
+    const  {error}=campgroundSchema.validate(req.body);
+ 
+    if(error){
+        const msg=error.details.map(el=>el.message).join(',');
+        throw new ExpressError(msg,400);
+    }
    const campground=new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
