c54f3e2d30349054f14523f9155f34f73d1a1eec
Aravind <aravindyedala49@gmail.com>
2021-07-14 12:02:03 +0530

    creating review model

--- models/campground.js
+++ models/campground.js
@@ -6,6 +6,12 @@ const CampgroundSchema=new Schema({
     image:String,
     price:Number,
     description:String,
-    location:String
+    location:String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
 });
 module.exports=mongoose.model('Campground',CampgroundSchema);
